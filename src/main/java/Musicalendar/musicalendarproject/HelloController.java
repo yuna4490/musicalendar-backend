1b4a3c916c016037fe5b960523eb873a7512dd40
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-04-10 16:57:23 +0900

    프론트-백 서버 연동 테스트용 코드

--- src/main/java/Musicalendar/musicalendarproject/HelloController.java
+++ src/main/java/Musicalendar/musicalendarproject/HelloController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 public class HelloController {
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public List<String> Hello(){
         return Arrays.asList("서버 포트는 8080", "리액트 포트는 3000");
     }
