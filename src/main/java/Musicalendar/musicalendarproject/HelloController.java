efe80b1a32a9839131077483cccb1f61cb04e6b6
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-04-10 16:58:15 +0900

    프론트-백 서버 연동 테스트용 코드

--- src/main/java/Musicalendar/musicalendarproject/HelloController.java
+++ src/main/java/Musicalendar/musicalendarproject/HelloController.java
@@ -1,6 +1,7 @@
 package Musicalendar.musicalendarproject;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -8,7 +9,7 @@ import java.util.List;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
+    @RequestMapping("/hello")
     public List<String> Hello(){
         return Arrays.asList("서버 포트는 8080", "리액트 포트는 3000");
     }
