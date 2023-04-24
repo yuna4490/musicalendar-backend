3fbeaae308abe1a683f2d67b666bab17d6be3880
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-04-24 21:26:05 +0900

    안쓰는 부분 일단 주석처리

--- src/main/java/Musicalendar/musicalendarproject/controller/ShowTestController.java
+++ src/main/java/Musicalendar/musicalendarproject/controller/ShowTestController.java
@@ -1,19 +1,19 @@
-package Musicalendar.musicalendarproject.controller;
-
-import Musicalendar.musicalendarproject.service.ShowService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-@RequiredArgsConstructor
-public class ShowTestController {
-
-    private final ShowService showService;
-
-    @GetMapping("/hello")
-    public void test(){
-        showService.saveTicketingData();
-    }
-
-}
+//package Musicalendar.musicalendarproject.controller;
+//
+//import Musicalendar.musicalendarproject.service.ShowService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//@RequiredArgsConstructor
+//public class ShowTestController {
+//
+//    private final ShowService showService;
+//
+//    @GetMapping("/hello")
+//    public void test(){
+//        showService.saveTicketingData();
+//    }
+//
+//}
